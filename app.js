7fe16ad705c7d16f52efb3febab2554145a21cc7
RownokRatul <1805019@ugrad.cse.buet.ac.bd>
2023-08-20 04:16:45 +0600

    get all diseases done

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(require('./routes/diagnosticsTabRoutes/getPrescribedTestsRoute'));
 app.use(require('./routes/diagnosticsTabRoutes/getPrescribedTestDetailsRoute'));
 
 app.use(require('./routes/searchRoutes/searchTabRoute'));
-
+app.use(require('./routes/createPrescriptionRoutes/get_all_diseases_Routes'));
 // const users = [
 //   { id: 1, username: 'john_doe', name: 'John Doe', email: 'john.doe@example.com' },
 //   { id: 2, username: 'jane_smith', name: 'Jane Smith', email: 'jane.smith@example.com' },
