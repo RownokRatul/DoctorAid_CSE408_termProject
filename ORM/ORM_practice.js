efad3bf12199e39209664cc3f4ca23083278ff9e
TokiTahmid64 <sharifulislamtoki@gmail.com>
2023-09-03 13:53:21 +0600

    online backend

--- ORM/ORM_practice.js
+++ ORM/ORM_practice.js
@@ -20,6 +20,7 @@ async function getPractice(){
     try{
         const practice = await prisma.practice.findMany({
             select: {
+                id: true,
                 name: true,
                 relations: {
                     select: {
@@ -35,9 +36,46 @@ async function getPractice(){
         throw error;
     }
 }
+// delete function
+async function deletePractice(practiceData){
+    try{
+        const deletePractice = await prisma.practice.delete({
+            where: {
+                id: practiceData.id,
+            } 
+        });
+
+        return deletePractice;
+    }catch(error){
+        console.error('Error deleting practice information:', error);
+        throw error;
+    }
+
+}
+// update function
+async function editPractice(practiceData){
+    try{
+        const editPractice = await prisma.practice.update({
+            where: {
+                id: practiceData.id,
+            },
+            data: {
+                name: practiceData.name,
+            } 
+        });
+
+        return editPractice;
+    }catch(error){
+        console.error('Error updating practice information:', error);
+        throw error;
+    }
+
+}
 
 module.exports = {
     createPractice,
     getPractice,
+    deletePractice,
+    editPractice,
 };
 
--- routes/practiceRoutes/practiceTabRoutes.js
+++ routes/practiceRoutes/practiceTabRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const orm_registerPatient = require('../../ORM/ORM_registerPatient');
 const cookie = require('../../authentication/cookie_checker');
 require('dotenv').config();
-const {getPracticeController, createPracticeController} = require('../../controllers/practice_controller/practiceController'); // Import the patientRegisterController function
+const {getPracticeController, createPracticeController, deletePracticeController, editPracticeController} = require('../../controllers/practice_controller/practiceController'); // Import the patientRegisterController function
 
 
 const router = express.Router();
@@ -36,5 +36,39 @@ router.post('/api/v0/create_practice', async (req, res) => { //post->get
         }
     }
 });
+
+//route to delete a practice
+router.delete('/api/v0/delete_practice', async (req, res) => { //post->get
+    {
+        console.log("req.body\n");
+        const pseudo_view = await deletePracticeController(req);
+        if(pseudo_view) {
+            console.log(pseudo_view);
+            res.status(200).json({message : 'Success', data : pseudo_view});
+        }
+        else {
+            console.log('NO user Found!');
+            res.status(404).json({error : 'Not Found'});
+        }
+    }
+});
+
+
+// router to update (edit) a practice
+router.put('/api/v0/update_practice', async (req, res) => { //post->get
+    {
+        console.log("req.body\n");
+        const pseudo_view = await editPracticeController(req);
+        if(pseudo_view) {
+            console.log(pseudo_view);
+            res.status(200).json({message : 'Success', data : pseudo_view});
+        }
+        else {
+            console.log('NO user Found!');
+            res.status(404).json({error : 'Not Found'});
+        }
+    }
+});
+
 
 module.exports = router;
